e4d9049ffbe29c944bcaff72ce4bfc0585f9d155
z510102 <201015@st.yoshida-g.ac.jp>
2022-11-11 18:25:39 +0900

    1111

---
+++ WeatherApp/app/src/main/java/com/websarva/wings/android/weatherapp/MainActivity.kt
@@ -0,0 +1,221 @@
+package com.websarva.wings.android.weatherapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import android.widget.TextView
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import androidx.core.os.HandlerCompat
+import com.websarva.wings.android.weatherapp.R
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val DEBUG_TAG = "AsyncSample"
+
+        private const val WEATHERINFO_URL = "https://api.openweathermap.org/data/2.5/weather?lang=ja"
+
+        private const val APP_ID = "d69cb3b99bca1385860aafd20c655c95"
+    }
+
+    @UiThread
+
+    private fun receiveWeatherInfo(urlFull: String) {
+        val handler = HandlerCompat.createAsync(mainLooper)
+        val backgroundReceiver = WeatherInfoBackgroundReceiver(handler, urlFull)
+        val executeService = Executors.newSingleThreadExecutor()
+        executeService.submit(backgroundReceiver)
+    }
+
+    private inner class WeatherInfoBackgroundReceiver(handler: Handler, url: String): Runnable {
+        private val _handler = handler
+
+        private val _url = url
+
+        @WorkerThread
+
+        override fun run() {
+            var result = ""
+
+            val url = URL(_url)
+
+            val con = url.openConnection() as? HttpURLConnection
+
+            con?.let {
+                try {
+                    it.connectTimeout = 1000
+
+                    it.readTimeout = 1000
+
+                    it.requestMethod = "GET"
+
+                    it.connect()
+
+                    val stream = it.inputStream
+
+                    result = is2String(stream)
+
+                    stream.close()
+                }
+
+                catch (ex: SocketTimeoutException) {
+                    Log.w(DEBUG_TAG, "通信タイムアウト", ex)
+                }
+
+                it.disconnect()
+            }
+
+            val postExecutor = WeatherInfoPostExecutor(result)
+            _handler.post(postExecutor)
+        }
+
+        private fun is2String(stream: InputStream): String {
+            val sb = StringBuilder()
+
+            val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
+
+            var line = reader.readLine()
+
+            while(line != null) {
+                sb.append(line)
+                line = reader.readLine()
+            }
+            reader.close()
+            return sb.toString()
+        }
+    }
+
+    private inner class WeatherInfoPostExecutor(result: String): Runnable {
+        private val _result = result
+
+        @UiThread
+
+        override fun run() {
+            val rootJSON = JSONObject(_result)
+
+            val cityName = rootJSON.getString("name")
+
+            val telop = "${cityName}の天気"
+
+
+
+            val weatherJSONArray = rootJSON.getJSONArray("weather")
+
+            val weatherJSON = weatherJSONArray.getJSONObject(0)
+
+            val weather = weatherJSON.getString("description")
+
+            val desc = "${weather}"
+
+            val descT = "${weather}"
+
+
+
+            val main = rootJSON.getJSONObject("main")
+
+            val max = "${main.getInt("temp_max")-273}℃ /"
+            val min = "${main.getInt("temp_min")-273}℃"
+
+            val maxT = "${main.getInt("temp_max")-273}℃ /"
+            val minT = "${main.getInt("temp_min")-273}℃"
+
+
+
+            val tvWeatherTelop = findViewById<TextView>(R.id.tvWeatherTelop)
+            val tvWeatherDesc = findViewById<TextView>(R.id.tvWeatherDesc)
+            val tvWeatherDescT = findViewById<TextView>(R.id.tvWeatherDescT)
+            val tvMax: TextView = findViewById(R.id.tvMax)
+            val tvMin: TextView = findViewById(R.id.tvMin)
+            val tvMaxT: TextView = findViewById(R.id.tvMaxT)
+            val tvMinT: TextView = findViewById(R.id.tvMinT)
+
+            tvWeatherTelop.text = telop
+            tvWeatherDesc.text = desc
+            tvWeatherDescT.text = descT
+            tvMax.text = max
+            tvMin.text = min
+            tvMaxT.text = maxT
+            tvMinT.text = minT
+        }
+    }
+
+    private var _list: MutableList<MutableMap<String, String>> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        _list = createList()
+
+        val lvCityList = findViewById<ListView>(R.id.lvCityList)
+        val from = arrayOf("name")
+        val to = intArrayOf(android.R.id.text1)
+        val adapter = SimpleAdapter(this@MainActivity, _list, android.R.layout.simple_list_item_1, from, to)
+        lvCityList.adapter = adapter
+        lvCityList.onItemClickListener = ListItemClickListener()
+    }
+
+    private fun createList(): MutableList<MutableMap<String, String>> {
+        val list: MutableList<MutableMap<String, String>> = mutableListOf()
+
+        var city = mutableMapOf("name" to "札幌", "q" to "Sapporo")
+        list.add(city)
+
+        city = mutableMapOf("name" to "東京", "q" to "Tokyo")
+        list.add(city)
+
+        city = mutableMapOf("name" to "横浜", "q" to "Yokohama")
+        list.add(city)
+
+        city = mutableMapOf("name" to "名古屋", "q" to "Nagoya")
+        list.add(city)
+
+        city = mutableMapOf("name" to "京都", "q" to "Kyoto")
+        list.add(city)
+
+        city = mutableMapOf("name" to "奈良", "q" to "Nara")
+        list.add(city)
+
+        city = mutableMapOf("name" to "大阪", "q" to "Osaka")
+        list.add(city)
+
+        city = mutableMapOf("name" to "神戸", "q" to "Kobe")
+        list.add(city)
+
+        city = mutableMapOf("name" to "広島", "q" to "Hiroshima")
+        list.add(city)
+
+        city = mutableMapOf("name" to "沖縄", "q" to "Okinawa")
+        list.add(city)
+
+        return list
+    }
+
+    private inner class ListItemClickListener: AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val item = _list.get(position)
+
+            val q = item.get("q")
+
+            q?.let {
+                val urlFull = "$WEATHERINFO_URL&q=$q&appid=$APP_ID"
+                receiveWeatherInfo(urlFull)
+            }
+        }
+    }
+}
